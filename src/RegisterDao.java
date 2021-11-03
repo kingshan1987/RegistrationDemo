b933a97ae776928043405ac54058217a49ddc970
shanmugamkrishnan <90957540+shanmugamkrishnan@users.noreply.github.com>
2021-11-03 18:03:24 +0530

    Update RegisterDao.java
    dbpassword has been updated as per the new Database schema and modified some variables name as per the requirement.

--- src/RegisterDao.java
+++ src/RegisterDao.java
@@ -1,60 +1,62 @@
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class RegisterDao {
+public class RegisterDao
+{
+	private String dburl = "jdbc:mysql://localhost:3306/userdb";
+	private String dbuname = "root";
+	private String dbpassword = "shanking143";
+	private String dbdriver = "com.mysql.jdbc.Driver";
 	
-	private String dbUrl = "jdbc:mysql://localhost:3306/userdb";
-	private String dbUname = "root";
-	private String dbPassword = "rootpasswordgiven";
-	private String dbDriver = "com.mysql.cj.jdbc.Driver";
-	
-	public void loadDriver(String dbDriver)
+	public void loadDriver(String dbdriver)
 	{
-		try {
-			Class.forName(dbDriver);
-		} catch (ClassNotFoundException e) {
+		try 
+		{
+			Class.forName(dbdriver);
+		} catch (ClassNotFoundException e) 
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public Connection getConnection()
 	{
-		Connection con = null;
-		try {
-			con = DriverManager.getConnection(dbUrl, dbUname, dbPassword);
-		} catch (SQLException e) {
+		Connection con=null;
+		try 
+		{
+			con = DriverManager.getConnection(dburl, dbuname, dbpassword);
+		} catch (SQLException e) 
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return con;
 	}
 	
-	
 	public String insert(Member member)
 	{
-		loadDriver(dbDriver);
+		loadDriver(dbdriver);
 		Connection con = getConnection();
-		String result = "Data entered successfully";
-		String sql = "insert into member values(?,?,?,?)";
-		
-		PreparedStatement ps;
-		try {
-		ps = con.prepareStatement(sql);
-		ps.setString(1, member.getUname());
-		ps.setString(2, member.getPassword());
-		ps.setString(3, member.getEmail());
-		ps.setString(4, member.getPhone());
-		ps.executeUpdate();
-		} catch (SQLException e) {
+		String result = "Data entered Successfully";
+		String sql="insert into userdb.member values (?,?,?,?)";
+		try 
+		{
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, member.getUname());
+			ps.setString(2, member.getPassword());
+			ps.setString(3, member.getEmail());
+			ps.setString(4, member.getPhone());
+			ps.executeUpdate();
+		} catch (SQLException e) 
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			result = "Data not entered";
+			result ="Data not entered";
 		}
 		return result;
 	}
-
 }
